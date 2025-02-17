64bfd83d1605048f5907857ab4367423c74c9441
unknown <darko.vidic2@gmail.com>
2025-02-17 19:09:01 +0100

    Fix image name

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function calculateDaysLeft(validUntil: string | Date): number {
-  const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+  const oneDay = 24 * 60 * 60 * 1000;
   const firstDate = new Date(validUntil);
   const secondDate = new Date();
 
